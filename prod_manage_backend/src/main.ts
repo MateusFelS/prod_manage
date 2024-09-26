496908165989d8e9918272644543da4b2ea5c2cd
Mateus Santos <122156760+MateusFelS@users.noreply.github.com>
2024-09-26 17:57:22 -0300

    Update main.ts

--- prod_manage_backend/src/main.ts
+++ prod_manage_backend/src/main.ts
@@ -5,7 +5,5 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const port = process.env.PORT || 3000; // Usa a porta definida pelo Vercel,
-  await app.listen(port);
 }
 bootstrap();
