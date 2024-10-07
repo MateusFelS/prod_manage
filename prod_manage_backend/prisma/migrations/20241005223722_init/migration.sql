190f7c4041ca12814281a6ea969d5e9ef5fbe5e1
Mateus Santos <122156760+MateusFelS@users.noreply.github.com>
2024-10-07 17:06:01 -0300

    delete migrations

--- prod_manage_backend/prisma/migrations/20241005223722_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `calculatedTime` on the `operationrecord` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `operationrecord` DROP COLUMN `calculatedTime`;
