dc5649ba3aac3dfc7f685e30ce0dbc7e93370084
Mateus Santos <122156760+MateusFelS@users.noreply.github.com>
2025-03-30 20:14:15 -0300

    Delete prod_manage_QA directory

--- prod_manage_QA/api_automation_testing/features/support/base_url.rb
+++
@@ -1,6 +0,0 @@
-module API
-    include HTTParty
-    base_uri 'https://prod-manage-backend.onrender.com' # Coloque sua url base
-    format :json
-    headers 'Content-Type': 'application/json'
-end
--- prod_manage_QA/api_automation_testing/features/support/env.rb
+++
@@ -1,6 +0,0 @@
-require "cucumber"
-require "httparty"
-require "httparty/request"
-require "httparty/response/headers"
-require "rspec"
-include RSpec::Matchers
